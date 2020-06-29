90fe031d9cd6d25da8ed10a33034100dbce3391c
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-06-29 18:39:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 module "my_vpc"{
 source = ".//modules/vpc"
+  
+# below are the variables present in the modules/vpc/vars.tf with no defined values there
+  
 region = "us-east-1"
 vpc_pub_az = ["us-east-1a","us-east-1b"]
 vpc_pri_az = ["us-east-1c","us-east-1d"]
