0a16393bfe01f0622ffb258024cfae3678ca2b15
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-01 18:47:28 +0530

    Update ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -5,6 +5,7 @@ ami = var.aws_ami
 subnet_id = element(var.mod_vpc_subs, count.index)
 instance_type = "t2.micro"
 key_name = var.key_name
+iam_instance_profile = var.mod_iam_name  
 user_data = file("${path.module}/httpd.sh")
 vpc_security_group_ids = var.mod_vpc_pub_sg
   tags = {
