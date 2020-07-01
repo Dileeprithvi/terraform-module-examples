21c5f813574bb929249e9bb66b5cd4bc4f1635aa
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-01 18:36:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
+module "global_variables" {
+  source= ".//modules/global_variables"
+}
+
+provider "aws" {
+  region= module.global_variables.aws_region
+}
+
+    
 module "my_vpc"{
 source = ".//modules/vpc"
 
 # Below are the variables present in the modules/vpc/vars.tf with no defined values there. Declaring the values below  
   
-region = "us-east-1"
 vpc_pub_az = ["us-east-1a","us-east-1b"]
 vpc_pri_az = ["us-east-1c","us-east-1d"]
 }
@@ -20,7 +28,6 @@ output "sg1" {
 
 module "my_ec2"{
 source = ".//modules/ec2"
-region = "us-east-1"
   
 # Below values in the format!! - module.(logical name of the module vpc---> my_vpc).{referencing the output logical names of modules/vpc/output.tf}
 # my_vpc.subnets is a list(string) because the output contains two public subnet values
