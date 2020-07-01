1400e1b92f2c9c9bc92fac7b3c9feded09c6a134
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-01 10:52:59 +0530

    Update ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,1 +1,14 @@
-will create
+resource "aws_instance" "web-instance" {
+# count = 2 referencing to deploy two instances in two public subnets with subnet_id  
+count = var.instance_count
+ami = var.aws_ami
+subnet_id = element(var.mod_vpc_subs, count.index)
+instance_type = "t2.micro"
+key_name = var.key_name
+user_data = file("${path.module}/httpd.sh")
+vpc_security_group_ids = var.mod_vpc_pub_sg
+  tags = {
+    Name = "web-instance-${count.index + 1}"
+  }
+}
+
