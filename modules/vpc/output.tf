ec3976d7ef1e40ec2eb95c77d4da069e75328f44
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-01 10:47:26 +0530

    Update output.tf

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,18 +1,32 @@
-# The below output describes all the two subnet ids
+# The below output describes all the two subnet ids without tuple form
 
 output "subnets" {
-  value = [aws_subnet.public.*.id]
+  value = aws_subnet.public.*.id
 }
 
+# The below output describes all the two subnet ids with tuple form
 
-# The below output describes first [0] subnet id
+#output "subnets" {
+ # value = [aws_subnet.public.*.id]
+#} --> Referencing one subnet to one aws ec2 ( modules/ec2/ec2.tf), then edit ec2.tf under subnet_id = var.mod_vpc_subs[0][0] or var.mod_vpc_subs[0][1] ---> the two array because of the output gives two subnets
+
+
+
+
+# The below output describes Second [1] subnet id
 
 output "subnets_single" {
-  value = [aws_subnet.public.0.id]
+  value = [aws_subnet.public.1.id]
 }
 
-# The below output describes the id of the VPC
+# The below output describes vpc id
 
 output "vpc_id" {
   value = [aws_vpc.terraform_vpc.id]
+}
+
+# The below output describes Public subnet group
+
+output "sg_pub_id" {
+  value = [aws_security_group.sg_public.id]
 }
