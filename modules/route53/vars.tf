171ab6e65397ff5428ffb2c426b7bcad49df548f
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-02 22:40:51 +0530

    Create vars.tf

---
+++ modules/route53/vars.tf
@@ -0,0 +1,7 @@
+variable "mod_alb_dns_name" {
+description = "DNS Name fetched from the modules/alb/alb.tf (output) - mentioned in main.tf"
+}
+
+variable "mod_alb_zone_id" {
+description = "Zone ID fetched from the modules/alb/alb.tf (output) - mentioned in main.tf"
+}
