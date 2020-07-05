8ce9d3131c783b64306859d6aad40b4908bceaa9
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-05 15:38:48 +0530

    Update output.tf

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -11,4 +11,11 @@ output "web_instance" {
   value = aws_instance.web-instance.*.id
 }
 */
+  
+output "web_instance-1" {
+  value = aws_instance.web-instance1.id
+}
 
+output "web_instance-2" {
+  value = aws_instance.web-instance2.id
+}
