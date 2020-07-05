4c9f528a8eeeb6c883a5768d66d90e358142a62b
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-05 13:45:21 +0530

    Update testing.sh

--- modules/ec2/testing.sh
+++ modules/ec2/testing.sh
@@ -2,5 +2,7 @@
 sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
-mkdir -p /var/www/html/testing/index.html
+mkdir -p /var/www/html/testing
+sudo cd /var/www/html/testing
+sudo touch index.thml
 echo "<h1>Terraform Web Instance Launched Successfully from Testing (Module Example)!!!!!</h1>" | sudo tee /var/www/html/testing/index.html
