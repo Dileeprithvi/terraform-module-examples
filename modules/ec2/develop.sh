531e93f4e24553ceb466dc10a7a009a66d24faf2
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-05 13:56:05 +0530

    Update develop.sh

--- modules/ec2/develop.sh
+++ modules/ec2/develop.sh
@@ -2,5 +2,7 @@
 sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
-mkdir -p /var/www/html/develop/index.html
+mkdir -p /var/www/html/develop
+sudo cd /var/www/html/develop
+sudo touch index.thml
 echo "<h1>Terraform Web Instance Launched Successfully from Develop (Module Example)!!!!!</h1>" | sudo tee /var/www/html/develop/index.html
