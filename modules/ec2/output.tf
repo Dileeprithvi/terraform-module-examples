372d836536967143c01961df87fdeaddf1eaf9bf
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-05 15:49:24 +0530

    Update output.tf

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -11,11 +11,12 @@ output "web_instance" {
   value = aws_instance.web-instance.*.id
 }
 */
-  
+ 
+# indices to be defined because the ec2.tf has the count value set to 1
 output "web_instance-1" {
-  value = aws_instance.web-instance1.id
+  value = aws_instance.web-instance1[0].id
 }
 
 output "web_instance-2" {
-  value = aws_instance.web-instance2.id
+  value = aws_instance.web-instance2[0].id
 }
