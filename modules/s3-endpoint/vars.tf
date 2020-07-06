88460dec7af60c8c5cccd04f0a916840e92f8829
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-06 15:49:35 +0530

    Create vars.tf

---
+++ modules/s3-endpoint/vars.tf
@@ -0,0 +1,7 @@
+variable "vpc" {
+description = "Define the value in main.yml taken from the modules/vpc/outputs"
+}
+
+variable "pri_route_id" {
+description = "Define the value in main.yml taken from the modules/vpc/outputs"
+}
