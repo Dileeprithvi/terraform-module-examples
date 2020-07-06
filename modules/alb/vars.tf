77870a9d31d39e4727d6b34236614440d88866d6
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-06 19:20:27 +0530

    Create vars.tf

---
+++ modules/alb/vars.tf
@@ -0,0 +1,27 @@
+variable "internal" {
+description = "Describing about the Internal or Internet faced Load balancer"
+  type        = bool
+  default     = false
+}
+
+
+variable "loadbalancer" {
+description = "Type of the loadbalancer"
+default = "application"
+}
+
+
+variable "mod_vpc_sg_value" {
+description = "Sg ID fetched from the modules/vpc/sg.tf (output) - mentioned in main.tf"
+}
+
+
+variable "mod_vpc_pub_subnet_value" {
+description = "Public subnets value fetched from modules/vpc/vpc.tf (output) - mentioned in main.tf "
+  type        = list(string)
+}
+
+
+variable "mod_vpc_id_value" {
+description = "VPC ID fetched from the modules/vpc/vpc.tf (output) - mentioned in main.tf"
+}
