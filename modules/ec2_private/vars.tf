9bf160427432c357b319f947ebb1c3c094925b33
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-07 10:07:00 +0530

    Create vars.tf

---
+++ modules/ec2_private/vars.tf
@@ -0,0 +1,28 @@
+variable "aws_ami" {
+description = "AWS region for hosting our your network"
+type = map
+    default = {
+    us-east-1 = "ami-09d95fab7fff3776c"
+    us-east-2 = "ami-06d5a8ce809b866ee"
+  }
+}
+
+variable "ec2_region" {
+}
+
+variable "instance_count" {
+default = "2"
+}
+
+variable "key_name" {
+description = "Key name for SSH into EC2"
+default = "awskey1"
+}
+  
+
+variable "ec2_tags" {
+  default = {
+    Createdby               = "Dileep Prithvi"
+    Role                    = "DevOps"
+  }
+}
