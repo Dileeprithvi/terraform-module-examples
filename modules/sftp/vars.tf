8bd039bd80f99f461ad438fc9792985f3d7d502b
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-07-18 12:11:56 +0530

    Create vars.tf

---
+++ modules/sftp/vars.tf
@@ -0,0 +1,10 @@
+variable "zone_id" {
+  description = "Route53 Zone ID"
+  type        = string
+  default     = ""
+}
+variable "domain_name" {
+  description = "CNAME Domain name"
+  type        = string
+  default     = ""
+}
